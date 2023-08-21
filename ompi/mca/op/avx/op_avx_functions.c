4b897e4c640df46c93f560f70d263c4b5ee9cb79
Weijia Song <songweijia@gmail.com>
2023-08-21 00:35:15 -0400

    +avx512detect

--- ompi/mca/op/avx/op_avx_functions.c
+++ ompi/mca/op/avx/op_avx_functions.c
@@ -187,6 +187,7 @@
 #if __AVX512F__
 #define OP_AVX_AVX512_FUNC(name, type_sign, type_size, type, op)               \
     if( OMPI_OP_AVX_HAS_FLAGS(OMPI_OP_AVX_HAS_AVX512F_FLAG|OMPI_OP_AVX_HAS_AVX512BW_FLAG) ) { \
+        ws_timing_punch(3001430,left_over,0);                                  \
         int types_per_step = (512 / 8) / sizeof(type);                         \
         for( ; left_over >= types_per_step; left_over -= types_per_step ) {    \
             __m512i vecA = _mm512_loadu_si512((__m512*)in);                    \
@@ -196,7 +197,7 @@
             _mm512_storeu_si512((__m512*)out, res);                            \
             out += types_per_step;                                             \
         }                                                                      \
-        printf("[triggered here]:%s:%d\n",__func__,__FILE__);                  \
+        ws_timing_punch(3001470,left_over,0);                                  \
         if( 0 == left_over ) return;                                           \
     }
 #else
@@ -256,7 +257,6 @@ static void OP_CONCAT(ompi_op_avx_2buff_##name##_##type,PREPEND)(const void *_in
 {                                                                              \
     int left_over = *count;                                                    \
     type *in = (type*)_in, *out = (type*)_out;                                 \
-    printf("[triggered here]:%s:%d\n",__func__,__FILE__);                      \
     OP_AVX_AVX512_FUNC(name, type_sign, type_size, type, op);                  \
     OP_AVX_AVX2_FUNC(name, type_sign, type_size, type, op);                    \
     OP_AVX_SSE4_1_FUNC(name, type_sign, type_size, type, op);                  \
